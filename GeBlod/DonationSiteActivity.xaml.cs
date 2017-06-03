a1e60668deffd074921741c78644f2cb2c9cb2a5
Henrik Norman <hnorma@kth.se>
2017-06-03 18:48:24 +0200

    Refactored to MVVM

--- GeBlod/DonationSiteActivity.xaml.cs
+++ GeBlod/DonationSiteActivity.xaml.cs
@@ -1,41 +1,11 @@
 ﻿
-using System;
-using System.Collections.ObjectModel;
-using System.Net.Http;
-using GeBlod.Models;
-using Newtonsoft.Json;
-
 namespace GeBlod
 {
     public partial class DonationSiteActivity
     {
-        private readonly string _url =
-            @"https://geblod.nu/wp-content/themes/default/api/?type=schedule&rid=13&d1=2017/05/20&d2=2017/05/29";
-
-        private readonly HttpClient _client = new HttpClient();
-        public ObservableCollection<DonationSite> DonationSites { get; } = new ObservableCollection<DonationSite>();
-
         public DonationSiteActivity()
         {
             InitializeComponent();
-            BindingContext = this;
-            LoadJson();
-        }
-
-        public async void LoadJson()
-        {
-            var response = await _client.GetStringAsync(_url);
-            var schemeResponse = JsonConvert.DeserializeObject<SchemeResponse>(response);
-            foreach (var donationSite in schemeResponse.Data)
-            {
-                DonationSites.Add(donationSite);
-            }
-        }
-
-        private void ListView_OnRefreshing(object sender, EventArgs e)
-        {
-            LoadJson();
-            //set IsRefreshing to false
         }
     }
 }
---
+++ GeBlod/ViewModels/DonationSiteActivityViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using GeBlod.Models;
+using Newtonsoft.Json;
+
+namespace GeBlod.ViewModels
+{
+    public class DonationSiteActivityViewModel
+    {
+        private readonly string _url =
+            @"https://geblod.nu/wp-content/themes/default/api/?type=schedule&rid=13&d1=2017/05/20&d2=2017/05/29";
+
+        private readonly HttpClient _client = new HttpClient();
+        public ObservableCollection<DonationSite> DonationSites { get; private set; } = new ObservableCollection<DonationSite>();
+
+        public DonationSiteActivityViewModel()
+        {
+            LoadJson();
+        }
+
+        public async void LoadJson()
+        {
+            var response = await _client.GetStringAsync(_url);
+            var schemeResponse = JsonConvert.DeserializeObject<SchemeResponse>(response);
+            foreach (var donationSite in schemeResponse.Data)
+            {
+                //DonationSites.Add(donationSite);
+            }
+            DonationSites = new ObservableCollection<DonationSite>(schemeResponse.Data);
+        }
+    }
+}
